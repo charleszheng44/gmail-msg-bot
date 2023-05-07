217ca982b71298474546c24f79ea2c9ac6fd49b1
Chao Zheng <charleszheng44@gmail.com>
2023-05-06 22:52:54 -0700

    fix query syntax bug

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 The Gmail bot is designed to monitor a designated Gmail account for incoming 
 email containing user-specified keywords, and send notifications to messaging 
 applications such as Lark and Slack.
+
+Please setup the necessary environment and auth by following [Python quickstart](https://developers.google.com/gmail/api/quickstart/python).
--- gmail_monitor.py
+++ gmail_monitor.py
@@ -3,6 +3,7 @@
 import os.path
 import base64
 import pickle
+import argparse
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
@@ -11,7 +12,7 @@ from googleapiclient.discovery import build
 # If modifying these SCOPES, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
 
-def get_credentials():
+def get_credentials(token_file):
     creds = None
     if os.path.exists('token.pickle'):
         with open('token.pickle', 'rb') as token:
@@ -21,17 +22,17 @@ def get_credentials():
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
+                token_file, SCOPES)
             creds = flow.run_local_server(port=0)
         with open('token.pickle', 'wb') as token:
             pickle.dump(creds, token)
     return creds
 
-def monitor_gmail_account(keywords):
-    creds = get_credentials()
+def monitor_gmail_account(keywords, token_file):
+    creds = get_credentials(token_file)
     service = build('gmail', 'v1', credentials=creds)
 
-    query = 'is:unread ' + ' '.join(f'({keyword})' for keyword in keywords)
+    query = 'is:unread ' + ' OR '.join(f'{keyword}' for keyword in keywords)
     results = service.users().messages().list(userId='me', q=query).execute()
     messages = results.get('messages', [])
 
@@ -56,6 +57,10 @@ def monitor_gmail_account(keywords):
         print('---')
 
 if __name__ == '__main__':
-    keywords = ['example_keyword1', 'example_keyword2']
-    monitor_gmail_account(keywords)
+    parser = argparse.ArgumentParser(description="Gmail bot")
+    parser.add_argument("-t", "--token", help="path to the gmail token file")
+    args = parser.parse_args()
+
+    keywords = ['google', 'test']
+    monitor_gmail_account(keywords, args.token)
 
